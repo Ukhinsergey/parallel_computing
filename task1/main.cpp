170a13c99cda79cd4acb92a49954c4378667ac71
serega <ser191097@gmail.com>
2020-10-05 00:09:43 +0300

    added function Fill

--- task1/main.cpp
+++ task1/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 
 void Generate(int Nx, int Ny, int k1, int k2, int &N, std::vector<int> &ia, std::vector<int> &ja){
@@ -55,55 +56,105 @@ int xysize(int Nx, int Ny,int k1,int k2) {
 
 void Generate1(int Nx, int Ny, int k1, int k2, int &N, std::vector<int> &ia, std::vector<int> &ja){
     N = (Nx + 1) * (Ny + 1);
-    ia.resize(N);
+    ia.resize(N + 1);
     int jasize = xysize(Nx, Ny, k1, k2);
-    // std::cout << jasize;
-    // return;
+    ia[N] = jasize;
     ja.resize(jasize);
-    int numsinrow = 0;
-    ia.push_back(0);
     for(int i = 0 ; i < N; ++i) {
         int r = i / (Nx + 1);
         int c = i % (Nx + 1);
+        int pos = 0;
+        if (r != 0) {
+            pos += 3 * 2 + (r - 1) * 4 * 2 + (Nx - 1) * 4 + (r - 1) * (Nx- 1) * 5;
+            if (c != 0 && r != Ny) {
+                pos += 4 + (c - 1) * 5;
+            } else if (c != 0) {
+                pos += 3 + (c - 1) * 4;
+            }
+        } else {
+            if (c != 0) {
+                pos += 3 + (c - 1) * 4;
+            } else {
+                pos = 0;
+            }
+        }
+        //^ pos without diag
+
+        //now upright 
+        if (r != 0) { 
+            int temp = (i - (Nx + 1) - (r - 1)) / (k1 + k2) * k2 + std::max(0, (i - (Nx + 1) - (r - 1)) % (k1 + k2) - k1);
+            pos += temp;
+        }
+
+        //now downleft
+        if (r != Ny) {
+            int c0 = 1;
+            if (c == 0 ) {
+                c0 = 0;
+            }
+            int temp = (i - r - c0) / (k1 + k2) * k2 + std::max(0, (i - r - c0) % (k1 + k2) -k1);
+            pos += temp;
+
+        } else {
+            int temp = (i - c - r) / (k1 + k2) * k2 + std::max(0, (i - r - c) % (k1 + k2) -k1);
+            pos += temp;
+        }
+
+
+        ia[i] = pos;
         //up
         if (r != 0) {
-            ja.push_back(i - (Nx + 1));
-            numsinrow++;
+            ja[pos++] = (i - (Nx + 1));
         }
         //upright
         if (r != 0 && c != Nx && ( ((r - 1) * Nx + c) % (k1 + k2) >= k1)) {
-            ja.push_back(i - Nx);
-            numsinrow++;
+            ja[pos++] = (i - Nx);
         }
         //left
         if (c != 0) {
-            ja.push_back(i - 1);
-            numsinrow++;
+            ja[pos++] = (i - 1);
         }
         //middle
-        ja.push_back(i);
-        numsinrow++;
+        ja[pos++] = (i);
         //right
         if (c != Nx) {
-            ja.push_back(i + 1);
-            numsinrow++;
+            ja[pos++] = (i + 1);
         }
         //downleft
         if (c != 0 && r != Ny && ((r * Nx + c - 1) % (k1 + k2) >= k1) ) {
-            ja.push_back(i + Nx);
-            numsinrow++;          
+            ja[pos++] =(i + Nx);          
         }
         //down
         if(r != Ny) {
-            ja.push_back(i + Nx + 1);
-            numsinrow++;
+            ja[pos++] = (i + Nx + 1);
         }
-        ia.push_back(numsinrow);
     }
-
 }
 
 
+void Fill(int N, std::vector<int> &ia, std::vector<int> &ja, std::vector<double> &A, std::vector<double> &b) {
+    A.resize(ia[N]);
+    b.resize(N);
+    int curpos = 0;
+    for(int i = 0 ; i < N; ++i) {
+        double sum = 0;
+        int pos = 0;
+        for(int j = ia[i]; j < ia[i + 1]; ++j) {
+            if (ja[j] == i) {
+                pos = curpos;
+                
+            } else {
+                A[curpos] = std::cos(i * ja[j] + i + ja[j]);
+                sum += std::abs(A[curpos]);
+                
+            }
+            curpos++;
+        }
+        sum *=1.234;
+        A[pos] = sum;
+        b[i] = std::sin(i);
+    }
+}
 
 int main(int argc, char **argv) {
     int Nx, Ny;
@@ -125,6 +176,9 @@ int main(int argc, char **argv) {
     std::vector<int> ja;
     int N;
     Generate1(Nx, Ny, k1, k2, N, ia, ja);
+    std::vector<double> A;
+    std::vector<double> b;
+    Fill(N, ia, ja, A, b);
     if (debug) {
         std::ofstream fout("output.txt");
         fout << "N = " << N << std::endl;
@@ -135,6 +189,16 @@ int main(int argc, char **argv) {
         fout << std::endl;
         fout << "IA : ";
         for(const auto &i: ia) {
+            fout << i << ' ';
+        }
+        fout << std::endl;
+        fout << "A : ";
+        for(const auto &i: A) {
+            fout << i << ' ';
+        }
+        fout << std::endl;
+        fout << "b : ";
+        for(const auto &i: b) {
             fout << i << ' ';
         }
         fout << std::endl;
